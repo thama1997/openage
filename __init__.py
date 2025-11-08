dd480ca59ea4754b206a43ef590a3d1eafca6dbf
thama1997 <thama1997@yahoo.com>
2025-11-08 17:16:28 +0100

    initial

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# Copyright 2015-2020 the openage authors. See copying.md for legal info.
+
+"""
+CMake build helpers; see doc/buildsystem.md for further info.
+
+Modules in the 'buildsystem' package are not intended to be installed
+with openage, or used from outside the build process; otherwise, they
+would be in the 'openage' package.
+"""
